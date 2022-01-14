e3683e26701348b9d8e1a3322416e143fd301d5e
eharn <77076617+eharn@users.noreply.github.com>
2022-01-14 08:48:51 -0700

    week2

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const http = require('http');
 const routes = require('./routes');
-console.log(routes.someText);
-const server = http.createServer(routes.handler);
+const express = require('express');
+const app = express();
+app.use((req, res, next) => {
+    console.log("in the middleware!");
+    next(); // allows request to continue to the next middlewere
+});
+
+app.use((req, res, next) => {
+    console.log("another middleware!");
+});
+
+const server = http.createServer(app);
 server.listen(3000);
--- routes/ta02.js
+++ routes/ta02.js
@@ -2,14 +2,45 @@
 // Remember, you can make more of these placeholders yourself!
 const express = require('express');
 const router = express.Router();
+const users = [{
+  username: 'username'
+},
+{
+  username: 'anothername'
+},
+{
+  username: 'andyetanother'
+}];
 
 router.get('/', (req, res, next) => {
-  res.render('pages', {
+  res.render('pages/ta02', {
     title: 'Team Activity 02',
     path: '/ta02', // For pug, EJS
     activeTA03: true, // For HBS
     contentCSS: true, // For HBS
+    userList: users,
   });
 });
 
-module.exports = router;
+router.post('/addUser', (req, res, next) => {
+  users.push({
+    username: req.body.username
+  });
+  res.redirect('/ta02');
+});
+
+router.post('/removeUser', (req, res, next) => {
+ 
+  let uIndex = users.findIndex(() => {
+    return req.body.username;
+  });
+  
+
+  users.splice(uIndex, 1);
+
+  res.redirect('/ta02');
+});
+
+
+exports.routes = router;
+exports.users = users;
