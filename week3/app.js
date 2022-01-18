9da16882a8694b066c3a4404d19e61a563d40c8a
eharn <77076617+eharn@users.noreply.github.com>
2022-01-18 16:40:32 -0700

    MVC

--- week3/app.js
+++ week3/app.js
@@ -2,64 +2,23 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
+
+const errorController = require('./controllers/error');
 
 const app = express();
-
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found' });
-});
+app.use(errorController.get404);
 
 app.listen(3000);
-
-
-// // const http = require('http');
-
-// // const routes = require('./routes');
-// const express = require('express');
-// const app = express();
-
-// const adminRoutes = require('./routes/admin');
-// const shopRoutes = require('./routes/shop');
-// const bodyParser = require('body-parser');
-
-// // app.use((req, res, next) => {
-// //     console.log('In the middleware!');
-// //     next(); // allows req to coninue to the next middlware in line
-// // });
-
-// app.use(adminRoutes);
-// app.use(shopRoutes);
-
-// app.use(bodyParser.urlencoded({extended: false}));
-
-// // app.use('/',(req, res, next) => {
-// //     console.log('This always runs');
-// //     next();
-// // });
-
-// // app.use('/add-product',(req, res, next) => {
-// //     console.log('In another middleware!');
-// //     res.send('<h1>Add product page</h1>'); //automatcially sets the type to html
-// //     //never want to add next as it should route somewhere else and we don't want to move it on
-// // });
-
-
-
-
-// // const server = http.createServer(app);
-
-// app.listen(3000);
